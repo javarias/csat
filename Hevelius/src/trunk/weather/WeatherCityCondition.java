f9020c4addc0c07b1c0c324ee53e25780fc579c2
mpilar <mpilar@75a0fe40-ac5b-440f-90d5-6af9f3fbf44f>
2007-06-22 20:14:52 +0000

    termino cosa de condiciones
    git-svn-id: https://csrg.inf.utfsm.cl/repos/software/CSAT@169 75a0fe40-ac5b-440f-90d5-6af9f3fbf44f

--- Hevelius/src/trunk/weather/WeatherCityCondition.java
+++ Hevelius/src/trunk/weather/WeatherCityCondition.java
@@ -3,13 +3,13 @@ import java.io.*;
 import java.util.*;
 import java.util.regex.*;
 
-public class WeatherCityId
+public class WeatherCityCondition
 {
 	private String time;
 	private String lat;
 	private String lon;
 	private String sunrice;
-	private String sinset;
+	private String sunset;
 	private String tzone;
 	private String tmp;
 	private String flik;
@@ -26,18 +26,116 @@ public class WeatherCityId
 	{
 		time=mat.group(1);
 	}
-	public void setId(Matcher mat)
+	public void setLat(Matcher mat)
 	{
-		time=mat.group(1);
+		lat=mat.group(1);
 	}
- 	public void setId(Matcher mat)
-	public void setId(Matcher mat)
-	public String getNameCity()
-        {
-                return name;
-        }
-        public String getId()
-        {
-                return id;
-        }
+ 	public void setLon(Matcher mat)
+	{
+		lon=mat.group(1);
+	}	
+	public void setSunrice(Matcher mat)
+	{
+		sunrice=mat.group(1);
+	}
+	public void sunset(Matcher mat)
+	{
+		sunset=mat.group(1);
+	}
+	public void setZone(Matcher mat)
+	{
+		tzone=mat.group(1);
+	}
+	public void setTm(Matcher mat)
+	{
+		tmp=mat.group(1);
+	}
+	public void setFlik(Matcher mat)
+	{
+		flik=mat.group(1);
+	}
+	public void setWt(Matcher mat)
+	{
+		wt=mat.group(1);
+	}
+	public void setPres(Matcher mat)
+	{
+		pres=mat.group(1);
+	}
+	public void setVwind(Matcher mat)
+	{
+		vwind=mat.group(1);
+	}
+	public void setGust(Matcher mat)
+	{
+		gust=mat.group(1);
+	}
+	public void setDirec(Matcher mat)
+	{
+		direc=mat.group(1);
+	}
+	public void setHumil(Matcher mat)
+	{
+		humil=mat.group(1);
+	}
+	public void setDpoint(Matcher mat)
+	{
+		dpoint=mat.group(1);
+	}
+	public void setMoon(Matcher mat)
+	{
+		moon=mat.group(1);
+	}
+	public String getTime()
+	{
+		return time;
+	}
+	public String getLat()
+	{
+		return lat;
+	}
+	public String getLon()
+	{
+		return lon;
+	}
+	public String getSunrice()
+	{
+		return sunrice;
+	}
+	public String getSunset(Matcher mat)
+	{
+		return sunset;
+	}
+	public String getTzone()
+	{
+		return tzone;
+	}
+	public String getTm()
+	{
+		return tmp;
+	}
+	public String getFlik()
+	{
+		return flik;
+	}
+	public String getWt()
+	{
+		return wt;
+	}
+	public String getPres()
+	{
+		return pres;
+	}
+	public String getVwind()
+	{
+		return vwind;
+	}
+	public String getGust()
+	{
+		return gust;
+	}
+	public String getDirec()
+	{
+		return direc;
+	}
 }
