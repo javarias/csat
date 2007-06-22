19980c4d44ce5fafbd6d1ea317aa954324a04926
cguajard <cguajard@75a0fe40-ac5b-440f-90d5-6af9f3fbf44f>
2007-06-22 17:02:22 +0000

    uniendo weas
    git-svn-id: https://csrg.inf.utfsm.cl/repos/software/CSAT@151 75a0fe40-ac5b-440f-90d5-6af9f3fbf44f

---
+++ Hevelius/src/trunk/interfaz/interfaz/Configuration.java
@@ -0,0 +1,75 @@
+import java.util.Properties;
+import java.io.*;
+
+public class Configuration extends Properties{
+	
+	static Properties propiedades;
+
+      
+      
+	static File file = new File("hevelius.properties");
+	static File dir = new File("~/.hevelius");
+	
+	public Configuration(){
+		
+		/*if(!dir.exists()){
+			dir.mkdir();
+		}*/
+				
+		if(!file.exists()){
+			try{
+				PrintWriter archivo = new PrintWriter(
+						new FileWriter("hevelius.properties",true));
+				
+				archivo.println("### Hevelius Configuration File ###");
+				archivo.println("");
+				archivo.println("#SYSTEM");
+				archivo.println("coordinate = 0");
+				archivo.println("location = null");
+				archivo.println("");
+				archivo.println("#INTERFACE");
+				archivo.println("#Modules");
+				archivo.println("weather = 1");
+				archivo.println("opengl = 1");
+				archivo.println("background = 0");
+				archivo.println("");
+				
+				archivo.close();
+								
+			}catch(IOException e){
+				System.out.println("Error al crear el archivo de Configuration");
+			}
+		}
+		
+		try{
+		    FileInputStream f = new FileInputStream("hevelius.properties");
+		    propiedades = new Properties();
+		    propiedades.load(f);
+		    f.close();
+			}catch(Exception e){
+				System.out.println("Error al cargar la Configuration");
+		}
+		
+	}
+
+	
+	
+	public String getOption(String a){
+		return propiedades.getProperty(a).trim();	
+	}
+
+	public void setOption(String a, String b){
+		propiedades.setProperty(a, b);
+	}
+
+	public void store(){
+		try{
+			OutputStream s = new FileOutputStream(new File("hevelius.properties"));
+			propiedades.store(s,"#HEVELIUS CONFIGURATION FILE");
+			s.close();
+		}catch(Exception e){
+			System.out.println("Error al Guadar las configuration");
+		}
+	}
+
+}
--- Hevelius/src/trunk/interfaz/interfaz/interfaz.java
+++ Hevelius/src/trunk/interfaz/interfaz/interfaz.java
@@ -5,18 +5,393 @@ import java.awt.event.*;
 public class interfaz {
 	public static JFrame frame;
 	public static DrawingPanel pane;
+
+
+
+	private static JDialog config;
+	private static JDialog about;
+	private static Configuration test = new Configuration();
+
+	private static JTabbedPane tabbedPane;
+	private static JPanel panel1;
+	private static JLabel coordtype;
+	private static JRadioButton radec;
+	private static JRadioButton altaz;
+	private static ButtonGroup coord;
+	private static JLabel city;
+	private static JComboBox location;
+
+	private static JPanel panel2;
+	private static JLabel modules;
+	private static JCheckBox weather;
+	private static JCheckBox opengl;
+	private static JLabel background;
+	private static JComboBox color;
+
+
+
+	static String[ ] fileItems = new String[ ] { "Exit" };
+	static String[ ] editItems = new String[ ] { "Configuration" };
+	static char[ ] fileShortcuts = { 'Q' };
+	static char[ ] editShortcuts = { 'P' };	
+
+	public static JMenuBar createMenuBar() {
+		JMenuBar menu = new JMenuBar();
+
+		JMenu fileMenu = new JMenu("File");
+		JMenu editMenu = new JMenu("Edit");
+		JMenu aboutMenu = new JMenu("Help");
+		JMenu subMenu = new JMenu("SubMenu");
+		JMenu subMenu2 = new JMenu("SubMenu2");
+
+		// Assemble the File menus with mnemonics.
+		ActionListener printListener = new ActionListener(  ) {
+			public void actionPerformed(ActionEvent event) {
+				if(event.getActionCommand().compareTo("Exit")==0){
+					System.exit(0);
+				}
+				else{
+					if(event.getActionCommand().compareTo("Configuration")==0){
+						//frame.setVisible(false);
+						config.setVisible(true); 
+					}
+					else{
+						if(event.getActionCommand().compareTo("About Hevelius")==0){
+							//frame.setVisible(false);
+							about.setVisible(true);
+						}
+						else{
+							System.out.println("Menu item [" + 
+									event.getActionCommand(  ) + "] was pressed.");
+						}
+					}
+				}
+			}
+		};
+		for (int i=0; i < fileItems.length; i++) {
+			JMenuItem item = new JMenuItem(fileItems[i], fileShortcuts[i]);
+			item.addActionListener(printListener);
+			fileMenu.add(item);
+		}
+
+		// Assemble the File menus with keyboard accelerators.
+		for (int i=0; i < editItems.length; i++) {
+			JMenuItem item = new JMenuItem(editItems[i]);
+			item.setAccelerator(KeyStroke.getKeyStroke(editShortcuts[i],
+						Toolkit.getDefaultToolkit(  ).getMenuShortcutKeyMask(  ), false));
+
+			/* if(item.getName().compareTo("Configuration") == 0){
+			   item.addActionListener(new ActionListener() {
+			   public void actionPerformed(ActionEvent e) {
+			   frame.setVisible(false);
+			   config.setVisible(true);
+			   }
+
+			   });
+			   }
+			   else{*/
+			item.addActionListener(printListener);
+			//}
+
+			editMenu.add(item);
+		}
+
+		// Insert a separator in the Edit menu in Position 1 after "Undo".
+		//editMenu.insertSeparator(1);
+
+		// Assemble the submenus of the Other menu.
+		JMenuItem item;
+		/*   subMenu2.add(item = new JMenuItem("Extra 2"));
+		     item.addActionListener(printListener);
+		     subMenu.add(item = new JMenuItem("Extra 1"));
+		     item.addActionListener(printListener);
+		     subMenu.add(subMenu2);
+
+		// Assemble the Other menu itself.
+		aboutMenu.add(subMenu);
+		aboutMenu.add(item = new JCheckBoxMenuItem("Check Me"));
+		item.addActionListener(printListener);
+		aboutMenu.addSeparconfigator(  );
+		ButtonGroup buttonGroup = new ButtonGroup(  );
+		aboutMenu.add(item = new JRadioButtonMenuItem("Radio 1"));
+		item.addActionListener(printListener);
+		buttonGroup.add(item);
+		aboutMenu.add(item = new JRadioButtonMenuItem("Radio 2"));
+		item.addActionListener(printListener);
+		buttonGroup.add(item);
+		aboutMenu.addSeparator(  );*/
+		aboutMenu.add(item = new JMenuItem("About Hevelius"));
+		item.addActionListener(printListener);
+
+		// Finally, add all the menus to the menu bar.
+		menu.add(fileMenu);
+		menu.add(editMenu);
+		menu.add(aboutMenu);
+
+
+		//   PropertiesConfiguration config = new PropertiesConfiguration("app.windows.properties");
+
+
+
+		return menu;
+	}
+
+
+
+	public static void configWindow() {
+
+
+		config = new JDialog(frame,"Hevelius - Configuration");
+		config.setLayout(null);
+		config.pack();	
+
+		config.addWindowListener(new WindowAdapter() {
+				public void windowClosing(WindowEvent e) {
+				config.setVisible(false);
+				}
+
+				public void windowClosed(WindowEvent e) {
+				config.setVisible(false);
+				}
+				});
+
+		config.setSize(500,600);
+		config.setLocationRelativeTo(null);
+
+		config.setResizable(false);
+
+		tabbedPane = new JTabbedPane();
+		tabbedPane.setSize(490,530);
+		tabbedPane.setLocation(0,0);
+
+		panel1 = new JPanel();
+		tabbedPane.addTab("System",panel1);
+		//tabbedPane.setMnemonicAt(0, KeyEvent.VK_1);
+		panel1.setLayout(null);
+
+
+		coordtype = new JLabel("Coordinate Selection: ");
+		coordtype.setLocation(10,10);
+		coordtype.setSize(150,20);
+		panel1.add(coordtype);
+
+		radec = new JRadioButton("RaDec");
+		altaz = new JRadioButton("Horizontal");
+		coord = new ButtonGroup();
+
+		if(Integer.parseInt(test.getOption("coordinate"))==0)
+			radec.setSelected(true);
+		if(Integer.parseInt(test.getOption("coordinate"))==1)
+			altaz.setSelected(true);
+		coord.add(radec);
+		coord.add(altaz);
+
+		radec.setLocation(150,13);
+		altaz.setLocation(150,33);
+		radec.setSize(150,15);
+		altaz.setSize(150,15);
+		panel1.add(radec);
+		panel1.add(altaz);		
+
+		city = new JLabel("Location: ");
+		city.setLocation(10,70);
+		city.setSize(150,20);
+		panel1.add(city);
+
+		location = new JComboBox();
+		location.setModel(new DefaultComboBoxModel(new String[]{
+					"Valparaíso/Viña del Mar",
+					"Santiago",
+					"La Serena",
+					"Tierra"}));
+		location.setLocation(150,70);
+		location.setSize(200,20);
+		location.setSelectedIndex(1);
+		panel1.add(location);
+
+
+
+
+
+		panel2 = new JPanel();
+		tabbedPane.addTab("Interface",panel2);
+		//tabbedPane.setMnemonicAt(1, KeyEvent.VK_2);
+		panel2.setLayout(null);
+
+		modules = new JLabel("Modules:");
+		modules.setLocation(10,10);
+		modules.setSize(70,20);
+		panel2.add(modules);
+
+		weather = new JCheckBox("Weather");
+		weather.setLocation(150,10);
+		weather.setSize(150,20);
+		if(Integer.parseInt(test.getOption("weather"))==1)
+			weather.setSelected(true);
+		panel2.add(weather);
+
+		opengl = new JCheckBox("OpenGL Model");
+		opengl.setLocation(150,30);
+		opengl.setSize(150,20);
+		if(Integer.parseInt(test.getOption("opengl"))==1)
+			opengl.setSelected(true);
+		panel2.add(opengl);
+
+
+		background = new JLabel("Background Color:");
+		background.setLocation(10,70);
+		background.setSize(150,20);
+		panel2.add(background);
+
+		color = new JComboBox();
+		color.setModel(new DefaultComboBoxModel(new String[]{
+					"Black",
+					"White",
+					"Red",
+					"Blue",
+					"Yellow",
+					"Grey",
+					"Green",
+					"Pink",
+					"Orange"}));
+		color.setLocation(150,70);
+		color.setSize(200,20);
+		color.setSelectedIndex(Integer.parseInt(test.getOption("background")));
+		panel2.add(color);		
+
+
+
+
+		/*	JPanel panel3 = new JPanel();
+			tabbedPane.addTab("Tab 3",panel3);
+		//tabbedPane.setMnemonicAt(2, KeyEvent.VK_3);
+		panel3.setLayout(null);
+		 */
+
+		config.add(tabbedPane);
+
+		JButton close = new JButton("Close");
+		JButton save = new JButton("Save");
+
+		close.setLocation(340,540);
+		close.setSize(70,25);
+		config.add(close);
+
+		save.setLocation(415,540);
+		save.setSize(70,25);
+		config.add(save);
+
+
+		close.addActionListener(new ActionListener(  ) {
+				public void actionPerformed(ActionEvent event) {
+				config.setVisible(false);
+				}
+				});
+
+		save.addActionListener(new ActionListener(  ) {
+				public void actionPerformed(ActionEvent event) {
+				//System.out.println(radec.isSelected() + " " + altaz.isSelected());
+				if(radec.isSelected())
+				test.setOption("coordinate","0");
+				if(altaz.isSelected())
+				test.setOption("coordinate","1");
+
+				if(weather.isSelected())
+				test.setOption("weather","1");
+				else
+				test.setOption("weather","0");
+
+				if(opengl.isSelected())
+				test.setOption("opengl","1");
+				else
+				test.setOption("opengl","0");
+
+
+
+				test.setOption("background", String.valueOf(color.getSelectedIndex()));
+
+				test.store();
+
+
+				}
+		});
+
+
+
+
+	}
+
+
+	public static void aboutWindow() {
+		about = new JDialog(frame,"Hevelius - About");
+		about.pack();	
+
+		about.addWindowListener(new WindowAdapter() {
+				public void windowClosing(WindowEvent e) {
+				about.setVisible(false);
+				}
+
+				public void windowClosed(WindowEvent e) {
+				about.setVisible(false);
+				}
+				});
+
+		about.setSize(400,400);
+		about.setLocationRelativeTo(null);
+		about.setLayout(null);
+		about.setResizable(false);
+
+
+		JLabel about1 = new JLabel("Hevelius v. ALFA rc1");
+		JLabel about2 = new JLabel("DevNull Enterprise");
+		JLabel about3 = new JLabel("2007");
+
+
+		about1.setLocation(100,15);
+		about1.setFont(new Font("DejaVu Sans",1,18));
+		about1.setSize(250,20);
+		about.add(about1);
+
+		about2.setLocation(150,30);
+		about2.setSize(150,20);
+		about.add(about2);
+
+		about3.setLocation(185,70);
+		about3.setSize(150,20);
+		about.add(about3);
+
+		JButton close = new JButton("Close");
+
+		close.setLocation(160,300);
+		close.setSize(70,25);
+		about.add(close);
+
+		close.addActionListener(new ActionListener(  ) {
+				public void actionPerformed(ActionEvent event) {
+				about.setVisible(false);
+				}
+				});
+	}
+
+
+
+
+
+
+
+
 	private static void createAndShowGUI()
 	{
 		frame = new JFrame("Hevelius v0.0.1");
 		pane = new DrawingPanel(null);
 		Dimension ScreenSize = Toolkit.getDefaultToolkit().getScreenSize();
 		frame.addWindowListener(new WindowAdapter()
-		{
-			public void windowClosing(WindowEvent e)
-			{
+				{
+				public void windowClosing(WindowEvent e)
+				{
 				System.exit(0);
-			}
-		});
+				}
+				});
 
 		pane.setBackground(Color.BLACK);
 
@@ -24,6 +399,7 @@ public class interfaz {
 		frame.pack();
 		frame.setSize(ScreenSize);
 		frame.setVisible(true);
+		frame.setJMenuBar(createMenuBar());
 		Dimension PanelSize = frame.getContentPane().getSize();
 		frame.setContentPane(pane);
 		pane.setSize(PanelSize);
@@ -47,7 +423,7 @@ public class interfaz {
 		coor.setSize(100,20);
 		coor.setForeground(Color.WHITE);
 		pane.add(coor);
-		
+
 		//RA or ALT Label
 		JLabel coor1L = new JLabel("X");
 		coor1L.setLocation(dist,260);
@@ -158,7 +534,7 @@ public class interfaz {
 		emp.setSize(20,recty+2);
 		emp.setMargin(new Insets(0,0,0,0));
 		pane.add(emp);
-		*/
+		 */
 
 		//Temperature Label
 		JLabel tempL = new JLabel("Temperature:");
@@ -250,12 +626,15 @@ public class interfaz {
 		westL.setForeground(Color.WHITE);
 		pane.add(westL);
 	}
+
 	public static void main(String[] args){
 		try {
 			UIManager.setLookAndFeel(
 					UIManager.getCrossPlatformLookAndFeelClassName());
 		} catch (Exception e) { }
 
+		configWindow();
 		createAndShowGUI();
+		aboutWindow();
 	}
 }
