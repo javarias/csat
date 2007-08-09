6d8ff6923099f3ecc3be69ec50ade5c6d14aa3c9
tstaig <tstaig@75a0fe40-ac5b-440f-90d5-6af9f3fbf44f>
2007-08-09 17:46:02 +0000

    git-svn-id: https://csrg.inf.utfsm.cl/repos/software/CSAT@275 75a0fe40-ac5b-440f-90d5-6af9f3fbf44f

--- Hevelius/src/trunk/Hevelius/acsmodules/pointing/PointingManual.java
+++ Hevelius/src/trunk/Hevelius/acsmodules/pointing/PointingManual.java
@@ -13,10 +13,12 @@ import alma.acs.exceptions.AcsJException;
 
 public class PointingManual extends ComponentImplBase implements SchedulerOperations, ComponentLifecycle{
 
-	private int offset1;
-	private int offset2;
+	private int offsetAlt;
+	private int offsetAz;
 	private int alt;
 	private int az;
+	private static boolean working;
+	private boolean stopping;
 
         private ContainerServices m_containerServices;
         private Logger m_logger;
@@ -25,44 +27,24 @@ public class PointingManual extends ComponentImplBase implements SchedulerOperat
         private alma.INSTRUMENT_MODULE.Instrument inst_comp;
         private alma.TELESCOPE_MODULE.Telescope tele_comp;
 
-	public PointingManual(int offset1, int offset2){
-		this.offset1=offset1;
-		this.offset2=offset2;
-	}
         //coneccion al modulo de tobar
 	 public void initialize(ContainerServices containerServices) throws ComponentLifecycleException {
                 m_containerServices = containerServices;
                 m_logger = m_containerServices.getLogger();
+		working = false;
+		stopping = false;
 
                 m_logger.finer("Lifecycle initialize() called");
 
                 // Get DataBase, Instrument, Telescope instances
                 org.omg.CORBA.Object obj = null;
                 try {
-                        obj = m_containerServices.getDefaultComponent("IDL:alma/DATABASE_MODULE/DataBase:1.0");
+                        obj = m_containerServices.getDefaultComponent("IDL:alma/TOBAR/tobi:1.0");
                         db_comp = alma.DATABASE_MODULE.DataBaseHelper.narrow(obj);
                 } catch (alma.JavaContainerError.wrappers.AcsJContainerServicesEx e) {
                         m_logger.fine("Failed to get DATABASE component reference " + e);
                         throw new ComponentLifecycleException("Failed to get DATABASE component reference");
                 }
-
-                try {
-                        obj = m_containerServices.getDefaultComponent("IDL:alma/INSTRUMENT_MODULE/Instrument:1.0");
-                        inst_comp = alma.INSTRUMENT_MODULE.InstrumentHelper.narrow(obj);
-                } catch (alma.JavaContainerError.wrappers.AcsJContainerServicesEx e) {
-                        m_logger.fine("Failed to get INSTRUMENT component reference " + e);
-                        throw new ComponentLifecycleException("Failed to get INSTRUMENT component reference");
-                }
-
-                try {
-                        obj = m_containerServices.getDefaultComponent("IDL:alma/TELESCOPE_MODULE/Telescope:1.0");
-                        tele_comp = alma.TELESCOPE_MODULE.TelescopeHelper.narrow(obj);
-                } catch (alma.JavaContainerError.wrappers.AcsJContainerServicesEx e) {
-                        m_logger.fine("Failed to get TELESCOPE component reference " + e);
-                        throw new ComponentLifecycleException("Failed to get TELESCOPE component reference");
-		}
-	}
-
 
 	public void execute() throws ComponentLifecycleException {
                 m_logger.finer("Lifecycle execute() called");
@@ -73,10 +55,6 @@ public class PointingManual extends ComponentImplBase implements SchedulerOperat
                 //Cleanup component references
                 if (db_comp != null)
                         m_containerServices.releaseComponent(db_comp.name());
-                if (inst_comp != null)
-                        m_containerServices.releaseComponent(inst_comp.name());
-                if (tele_comp != null)
-                        m_containerServices.releaseComponent(tele_comp.name());
         }
 
         public void aboutToAbort() {
@@ -108,10 +86,21 @@ public class PointingManual extends ComponentImplBase implements SchedulerOperat
                 return m_containerServices.getName();
         }
 
-	//tobar component
+	//MODULOS PARA MOVER
+	
+	void AltitudeOffSet(double degree)
+	{
+		offsetAlt=degree;
+	}
+	void AzimuthOffSet(double degree)
+	{
+		offsetAz=degree;
+	}
+	
 	//SACAR POSICIONES ACTUAL DEL TELESCOPIO
-	alt=component.alt+offset1;
-	az=component.az+offset2;
-	component_tobar(alt, az)
+	//getPos(RadecPos r, AltAz a)
+	//alt=component.getPos();
+	//az=component.az+offsetAz;
+	
 	}
  
