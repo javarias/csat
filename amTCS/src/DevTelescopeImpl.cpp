967e37d7e789ca26716035b0a054316ea4a76855
rtobar <rtobar@75a0fe40-ac5b-440f-90d5-6af9f3fbf44f>
2007-08-30 02:32:51 +0000

    - Added DevCCD idl  - Updated DevTelescopeImpl to the new IDL (CDB is still missing)
    git-svn-id: https://csrg.inf.utfsm.cl/repos/software/CSAT@348 75a0fe40-ac5b-440f-90d5-6af9f3fbf44f

--- amTCS/src/DevTelescopeImpl.cpp
+++ amTCS/src/DevTelescopeImpl.cpp
@@ -1,71 +1,85 @@
 #include <vltPort.h>
 static char *rcsId="@(#) $Id: $";
 static void *use_rcsId = ((void)&use_rcsId,(void *) &rcsId);
-#include <baciDB.h>
 
 #include <DevTelescopeImpl.h>
 
 using namespace baci;
 
-DevTelescopeImpl::DevTelescopeImpl(const ACE_CString& name, maci::ContainerServices *
-           containerServices):
-           CharacteristicComponentImpl(name,containerServices)
-           ,m_azimuth_sp(this)
-           ,m_altitude_sp(this)
+/* Constructor */
+DevTelescopeImpl::DevTelescopeImpl(const ACE_CString& name, maci::ContainerServices *containerServices) :
+       CharacteristicComponentImpl(name,containerServices)
+      ,m_realAzm_sp(this)
+      ,m_realAlt_sp(this)
 {
 	component_name = name.c_str();
 	ACS_TRACE("DevTelescopeImpl::DevTelescopeImpl");
 }
 
+/* Destructor */
 DevTelescopeImpl::~DevTelescopeImpl(){
 }
 
+/* Component Lifecycle */
 void DevTelescopeImpl::initialize() throw (acsErrTypeLifeCycle::LifeCycleExImpl)
 {
 	ACS_TRACE("DevTelescopeImpl::initialize");
 	if( getComponent() != 0){
-		m_azimuth_sp = new ROdouble( (component_name + std::string(":azimuth")).c_str(),
+		m_realAzm_sp = new ROdouble( (component_name + std::string(":azimuth")).c_str(),
 		                             getComponent());
-		m_altitude_sp = new ROdouble( (component_name + std::string(":altitude")).c_str(),
+		m_realAlt_sp = new ROdouble( (component_name + std::string(":altitude")).c_str(),
 		                             getComponent());
 	}
 }
 
-void DevTelescopeImpl::execute() throw (acsErrTypeLifeCycle::LifeCycleExImpl)
-{
-	ACS_TRACE("DevTelescopeImpl::execute");
+
+/* IDL implementation */
+void DevTelescopeImpl::setCurrentAltAz(const TYPES::AltazPos &p) throw (CORBA::SystemException){
+	ACS_TRACE("DevTelescopeImpl::setCurrentAlzAz");
 }
 
-void DevTelescopeImpl::cleanUp(){
+void DevTelescopeImpl::setVel(const TYPES::AltazVel &vel) throw (CORBA::SystemException){
 }
 
-void DevTelescopeImpl::aboutToAbort(){
+void DevTelescopeImpl::lock() throw (CORBA::SystemException){
+	//locking = true;
 }
 
-void DevTelescopeImpl::goTo(TYPES::AltazPos p){
-	ACS_TRACE("DevTelescopeImpl::goTo");
+void DevTelescopeImpl::unlock() throw (CORBA::SystemException){
+	//locking = false;
 }
 
-ACS::ROdouble_ptr DevTelescopeImpl::azimuth() throw (CORBA::SystemException){
-	if( m_azimuth_sp == 0 ){
+
+/* Attributes returning */
+TYPES::AltazVel DevTelescopeImpl::velocity() throw (CORBA::SystemException){
+	return m_velocity;
+}
+
+bool DevTelescopeImpl::locking() throw (CORBA::SystemException){
+	return m_locking;
+}
+
+/* Properties returning */
+
+ACS::ROdouble_ptr DevTelescopeImpl::realAzm() throw (CORBA::SystemException){
+	if( m_realAzm_sp == 0 ){
 		return ACS::ROdouble::_nil();
 	}
-	ACS::ROdouble_var prop = ACS::ROdouble::_narrow(m_azimuth_sp->getCORBAReference());
+	ACS::ROdouble_var prop = ACS::ROdouble::_narrow(m_realAzm_sp->getCORBAReference());
 	return prop._retn();
 }
 
-ACS::ROdouble_ptr DevTelescopeImpl::altitude() throw (CORBA::SystemException){
-	if( m_azimuth_sp == 0 ){
+ACS::ROdouble_ptr DevTelescopeImpl::realAlt() throw (CORBA::SystemException){
+	if( m_realAlt_sp == 0 ){
 		return ACS::ROdouble::_nil();
 	}
-	ACS::ROdouble_var prop = ACS::ROdouble::_narrow(m_altitude_sp->getCORBAReference());
+	ACS::ROdouble_var prop = ACS::ROdouble::_narrow(m_realAlt_sp->getCORBAReference());
 	return prop._retn();
 }
 
-///* --------------- [ MACI DLL support functions ] -----------------*/
-//#include <maciACSComponentDefines.h>
-//MACI_DLL_SUPPORT_FUNCTIONS(DevTelescopeImpl)
-///* ----------------------------------------------------------------*/
-  
-  
-  /*___oOo___*/
+/* --------------- [ MACI DLL support functions ] -----------------*/
+#include <maciACSComponentDefines.h>
+MACI_DLL_SUPPORT_FUNCTIONS(DevTelescopeImpl)
+/* ----------------------------------------------------------------*/
+
+/*___oOo___*/
