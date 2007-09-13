5a01c0c9a2f338e0af1f4b3e14fcd10d7d1fd2fe
mpilar <mpilar@75a0fe40-ac5b-440f-90d5-6af9f3fbf44f>
2007-09-13 19:07:42 +0000

    git-svn-id: https://csrg.inf.utfsm.cl/repos/software/CSAT@414 75a0fe40-ac5b-440f-90d5-6af9f3fbf44f

---
+++ Hevelius/src/trunk/amTCS/include/NexstarAltDevIO.h
@@ -0,0 +1,32 @@
+#ifndef _NEXSTAR_ALT_DEVIO_H_
+#define _NEXSTAR_ALT_DEVIO_H_
+
+#include <baciDevIO.h>
+#include <acstime.h>
+
+#include "SerialRS232.h"
+
+#define MAX_PRECISE_ROTATION   4294967296.0
+
+class NexstarAltDevIO: public DevIO<CORBA::Double>
+{
+
+	public:
+
+	NexstarAltDevIO(char *deviceName);
+	NexstarAltDevIO(void *data);
+	virtual ~NexstarAltDevIO();
+
+	virtual bool initializeValue();
+
+	CORBA::Double read(ACS::Time &timestamp)
+	              throw (ACSErr::ACSbaseExImpl);
+
+	void write(const CORBA::Double &value, ACS::Time &timestap)
+	              throw (ACSErr::ACSbaseExImpl);
+
+	private:
+	SerialRS232 *sp;
+};
+
+#endif /* _NEXSTAR_ALT_DEVIO_H_ */
