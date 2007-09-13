767b33c8a499693ad178b2f985f7ce11bdfb6af4
mpilar <mpilar@75a0fe40-ac5b-440f-90d5-6af9f3fbf44f>
2007-09-13 18:53:36 +0000

    git-svn-id: https://csrg.inf.utfsm.cl/repos/software/CSAT@413 75a0fe40-ac5b-440f-90d5-6af9f3fbf44f

---
+++ Hevelius/src/trunk/amTCS/src/NexstarAzmDevIO.cpp
@@ -0,0 +1,39 @@
+#include "NexstarAzmDevIO.h"
+
+NexstarAzmDevIO::NexstarAzmDevIO(char *deviceName) {
+	this->sp = new SerialRS232(deviceName);
+	this->sp->flush_RS232();
+}
+
+NexstarAzmDevIO::NexstarAzmDevIO(void *data) {
+}
+
+NexstarAzmDevIO::~NexstarAzmDevIO() {
+	delete this->sp;
+}
+
+bool NexstarAzmDevIO::initializeValue() {
+	return true;
+}
+
+CORBA::Double NexstarAzmDevIO::read(ACS::Time &timestamp) throw (ACSErr::ACSbaseExImpl) {
+
+	CORBA::Double azm(0.0);
+	char *msg;
+	unsigned long read_alt, read_azm;
+
+	/* Send the message to the telescope */
+	this->sp->write_RS232("z",1);
+	msg = this->sp->read_RS232();
+	sscanf(msg,"%08lX,%08lX#",&read_azm, &read_alt);
+	azm = read_azm / MAX_PRECISE_ROTATION;
+	azm *= 360.0;
+
+	return azm;
+}
+
+void NexstarAzmDevIO::write(const CORBA::Double &value, ACS::Time &timestamp) throw (ACSErr::ACSbaseExImpl){
+	ACS_SHORT_LOG((LM_ERROR, "NexstarAzmDevIO::write: This method should never be called!"));
+	return;
+}
+
---
+++ Hevelius/src/trunk/amTCS/src/lpiImpl.cpp
@@ -0,0 +1,86 @@
+#include <vltPort.h>
+static char *rcsId="@(#) $Id: $";
+static void *use_rcsId = ((void)&use_rcsId,(void *) &rcsId);
+
+#include <lpiImpl.h>
+
+using namespace baci;
+
+/* Constructor */
+lpiImpl::lpiImpl(const ACE_CString& name, maci::ContainerServices *containerServices) :
+       CharacteristicComponentImpl(name,containerServices)
+      ,m_frame_sp(this)
+{
+	component_name = name.c_str();
+	ACS_TRACE("lpiImpl::lpiImpl");
+	m_device = "/dev/video0";
+	m_locking = true;
+}
+
+/* Destructor */
+lpiImpl::~lpiImpl(){
+}
+
+/* Component Lifecycle */
+void lpiImpl::initialize() throw (acsErrTypeLifeCycle::LifeCycleExImpl)
+{
+	ACS_TRACE("lpiImpl::initialize");
+	if( getComponent() != 0){
+		m_frame_sp = new ROlongSeq( (component_name
+		                             + std::string(":frame")).c_str(),
+		                             getComponent());
+	}
+}
+
+
+/* IDL implementation */
+
+TYPES::Image* lpiImpl::image(CORBA::Double exposure) throw (CORBA::SystemException){
+	return new TYPES::Image();
+}
+
+void lpiImpl::lock() throw (CORBA::SystemException){
+	m_locking = true;
+}
+
+void lpiImpl::unlock() throw (CORBA::SystemException){
+	m_locking = false;
+}
+
+void lpiImpl::on() throw (CORBA::SystemException){
+	m_powered = true;
+}
+
+void lpiImpl::off() throw (CORBA::SystemException){
+	m_powered = false;
+}
+
+/* Attributes returning */
+char* lpiImpl::device() throw (CORBA::SystemException){
+	return m_device;
+}
+
+bool lpiImpl::locking() throw (CORBA::SystemException){
+	return m_locking;
+}
+
+bool lpiImpl::powered() throw (CORBA::SystemException){
+	return m_powered;
+}
+
+/* Properties returning */
+ACS::ROlongSeq_ptr lpiImpl::frame() throw (CORBA::SystemException){
+	if( m_frame_sp == 0 ){
+		return ACS::ROlongSeq::_nil();
+	}
+	ACS::ROlongSeq_var prop = ACS::ROlongSeq::_narrow(m_frame_sp->getCORBAReference());
+	return prop._retn();
+}
+
+
+/* --------------- [ MACI DLL support functions ] -----------------*/
+#include <maciACSComponentDefines.h>
+MACI_DLL_SUPPORT_FUNCTIONS(lpiImpl)
+/* ----------------------------------------------------------------*/
+
+/*___oOo___*/
