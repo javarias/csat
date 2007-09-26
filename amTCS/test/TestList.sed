cd5a866a3301a70937ee153c6d2ccc5a7057124d
rtobar <rtobar@75a0fe40-ac5b-440f-90d5-6af9f3fbf44f>
2007-09-26 04:08:04 +0000

    - Added some complexity to the test
    git-svn-id: https://csrg.inf.utfsm.cl/repos/software/CSAT@439 75a0fe40-ac5b-440f-90d5-6af9f3fbf44f

---
+++ amTCS/test/TestList.sed
@@ -0,0 +1,1 @@
+s/My IP is [0-9]\+.[0-9]\+.[0-9]\+.[0-9]\+/My IP is xxx.xxx.xxx.xxx/g
--- amTCS/test/tatTest.sh
+++ amTCS/test/tatTest.sh
@@ -2,4 +2,5 @@
 
 echo "This is my first tat test"
 echo "This should be exiting :D"
+echo "My IP is $(/sbin/ifconfig  | grep "inet addr" | grep Bcast | cut -d: -f2 | cut -d" " -f1)"
 exit 0
