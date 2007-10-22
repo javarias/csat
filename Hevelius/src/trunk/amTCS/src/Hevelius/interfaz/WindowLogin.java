ce9d5bed19ab9c4f044c7c18aed45504ab104d77
mpilar <mpilar@75a0fe40-ac5b-440f-90d5-6af9f3fbf44f>
2007-10-22 17:09:18 +0000

    git-svn-id: https://csrg.inf.utfsm.cl/repos/software/CSAT@545 75a0fe40-ac5b-440f-90d5-6af9f3fbf44f

--- Hevelius/src/trunk/amTCS/src/Hevelius/interfaz/WindowLogin.java
+++ Hevelius/src/trunk/amTCS/src/Hevelius/interfaz/WindowLogin.java
@@ -3,8 +3,10 @@ package Hevelius.interfaz;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.io.*;
 
 import java.util.*;
+import java.util.regex.*;
 import Hevelius.weather.*;
 import Hevelius.utilities.historial.*;
 
@@ -17,9 +19,13 @@ public class WindowLogin extends JDialog
 	private JTextField jTextField1;
 	private JButton set;
 
+
+	Pattern pat;
+	Matcher mat;
+
 	private int dx = 0;
 	private int dy = 0;
-	
+	private String name;
 	private static Configuration conf = new Configuration();
 	private static Historial hist = new Historial();		
 
@@ -33,16 +39,16 @@ public class WindowLogin extends JDialog
 
 		//System.out.println("LOGIN COOL");
 		//JDialog window = new JDialog(interfaz.getMainFrame(),"Hevelius - Login");
-                setLayout(null);
-                pack();
+		setLayout(null);
+		pack();
 
-                getContentPane().setBackground(Color.BLACK);
+		getContentPane().setBackground(Color.BLACK);
 
-                setSize(335,141);
-                setLocationRelativeTo(null);
+		setSize(335,141);
+		setLocationRelativeTo(null);
 		//setBackground(Color.BLACK);
 
-                setResizable(false);
+		setResizable(false);
 
 		//Usser Label
 		usser = new JLabel("username:");
@@ -50,52 +56,68 @@ public class WindowLogin extends JDialog
 		usser.setForeground(Color.WHITE);
 		usser.setFont(usser.getFont().deriveFont(15.0f));
 		add(usser);
-		
+
 		//text field
-		
+
 		jTextField1 = new JTextField();
 		jTextField1.setForeground(new Color(0, 0, 0 ));
-                jTextField1.setSelectedTextColor(new Color(0, 0, 0));
+		jTextField1.setSelectedTextColor(new Color(0, 0, 0));
 		jTextField1.setSize(130,20);
+		//jTextField1.setText("");
 		jTextField1.addActionListener(new ActionListener() {
-                                public void actionPerformed(ActionEvent e)
-                                {
-                                //jTextField1_actionPerformed(e);
-/*					conf.setOption("user",jTextField1.getText());
-                                        conf.store();
-					hist.addHistoryLogin(jTextField1.getText());
-					setVisible(false);
-*/
-					if(interfaz.getDrawingPanel().getCSATControl() == null && interfaz.getDrawingPanel().getCSATStatus() == null){
-					conf.setOption("user",jTextField1.getText());
-                                        conf.store();
-                                        hist.addHistoryLogin(jTextField1.getText());
-//                                        setVisible(false);
+				public void actionPerformed(ActionEvent e)
+				{
+				//jTextField1_actionPerformed(e);
+				/*					conf.setOption("user",jTextField1.getText());
+									conf.store();
+									hist.addHistoryLogin(jTextField1.getText());
+									setVisible(false);
+				 */					
+				name = jTextField1.getText();
+				name = name.trim();
+				pat = Pattern.compile("(\\S*)(\\s*)(\\S)");
+				System.out.println("NAME ="+name);
+				mat = pat.matcher(name);
+				System.out.println("Matcher ="+mat.find());
+				if(!mat.find()){
+
+				System.out.println("Nombre de Login =" + name);
+				if(interfaz.getDrawingPanel().getCSATControl() == null && interfaz.getDrawingPanel().getCSATStatus() == null ){
+				conf.setOption("user",jTextField1.getText());
+				conf.store();
+				hist.addHistoryLogin(jTextField1.getText());
+				//                                        		setVisible(false);
 
 
-						try
-		                                {
-		                                        interfaz.getDrawingPanel().setCSATControl(CSATControlClient.start());
-		                                        interfaz.getDrawingPanel().setCSATStatus(CSATStatusClient.start());
-		
-		                                        if(interfaz.getDrawingPanel().getCSATControl() != null && interfaz.getDrawingPanel().getCSATStatus() != null){
-		                                                interfaz.getDrawingPanel().getTelStatusPanel().setTrackingState(1);
-		                                                interfaz.getDrawingPanel().getTelStatusPanel().setPointingState(1);
-		                                                interfaz.getDrawingPanel().getTelStatusPanel().setPresettingState(1);
-		                                        }
-		                                }
-		                                catch(Exception err)
-		                                {
-		                                        err.printStackTrace();
-		                                }
+				try
+				{
+					interfaz.getDrawingPanel().setCSATControl(CSATControlClient.start());
+					interfaz.getDrawingPanel().setCSATStatus(CSATStatusClient.start());
+
+					if(interfaz.getDrawingPanel().getCSATControl() != null && interfaz.getDrawingPanel().getCSATStatus() != null){
+						interfaz.getDrawingPanel().getTelStatusPanel().setTrackingState(1);
+						interfaz.getDrawingPanel().getTelStatusPanel().setPointingState(1);
+						interfaz.getDrawingPanel().getTelStatusPanel().setPresettingState(1);
 					}
+				}
+				catch(Exception err)
+				{
+					err.printStackTrace();
+				}
+				}
+				}
+				else{
+					System.out.println("NO CARGO ACS CTM !!");
 
-					setVisible(false);
+					//init();
+
+				}
+				setVisible(false);
 
 
-                                }
+				}
 
-                                });
+		});
 
 		add(jTextField1);
 
@@ -108,27 +130,37 @@ public class WindowLogin extends JDialog
 		set.addActionListener(new ActionListener(  ) {
 				public void actionPerformed(ActionEvent event) {
 
+
+
+				name = jTextField1.getText();
+				name = name.trim();
+				pat = Pattern.compile("(\\S*)(\\s*)(\\S)");
+				System.out.println("NAME ="+name);
+				mat = pat.matcher(name);
+				System.out.println("Matcher ="+mat.find());
+				if(!mat.find()){
+
 				if(interfaz.getDrawingPanel().getCSATControl() == null && interfaz.getDrawingPanel().getCSATStatus() == null){
 
 				/* conf.setOption("user",jTextField1.getText());
 				   conf.store();
 				   hist.addHistoryLogin(jTextField1.getText());*/
 
-					try
-					{
-						interfaz.getDrawingPanel().setCSATControl(CSATControlClient.start());
-						interfaz.getDrawingPanel().setCSATStatus(CSATStatusClient.start());
+				try
+				{
+					interfaz.getDrawingPanel().setCSATControl(CSATControlClient.start());
+					interfaz.getDrawingPanel().setCSATStatus(CSATStatusClient.start());
 
-						if(interfaz.getDrawingPanel().getCSATControl() != null && interfaz.getDrawingPanel().getCSATStatus() != null){
-							interfaz.getDrawingPanel().getTelStatusPanel().setTrackingState(1);
-							interfaz.getDrawingPanel().getTelStatusPanel().setPointingState(1);
-							interfaz.getDrawingPanel().getTelStatusPanel().setPresettingState(1);
-						}
+					if(interfaz.getDrawingPanel().getCSATControl() != null && interfaz.getDrawingPanel().getCSATStatus() != null){
+						interfaz.getDrawingPanel().getTelStatusPanel().setTrackingState(1);
+						interfaz.getDrawingPanel().getTelStatusPanel().setPointingState(1);
+						interfaz.getDrawingPanel().getTelStatusPanel().setPresettingState(1);
 					}
-					catch(Exception err)
-					{
-						err.printStackTrace();
-					}
+				}
+		catch(Exception err)
+		{
+			err.printStackTrace();
+		}
 				}
 
 
@@ -137,20 +169,28 @@ public class WindowLogin extends JDialog
 				hist.addHistoryLogin(jTextField1.getText());
 				System.out.println(jTextField1.getText());
 				//jTextField1.setText(" ");
+				}
+				else{
+					System.out.println("NO CARGO ACS CTM !!");
+
+					//init();
+
+				}
+
 				setVisible(false);
 				}
-				});
+		});
 		add(set);
-		
+
 
 
 
 
 		usser.setLocation(50,20);
 
-               	jTextField1.setLocation(150,20);
+		jTextField1.setLocation(150,20);
 
-              	set.setLocation(150,70);
+		set.setLocation(150,70);
 
 
 		//setVisible(true);
