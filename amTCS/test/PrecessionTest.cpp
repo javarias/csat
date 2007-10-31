9f67dfe7c155db34fbe5e23204a653747162ca95
rtobar <rtobar@75a0fe40-ac5b-440f-90d5-6af9f3fbf44f>
2007-10-31 16:18:14 +0000

    - Done with the precession tests
    git-svn-id: https://csrg.inf.utfsm.cl/repos/software/CSAT@618 75a0fe40-ac5b-440f-90d5-6af9f3fbf44f

--- amTCS/test/PrecessionTest.cpp
+++ amTCS/test/PrecessionTest.cpp
@@ -9,6 +9,7 @@ using namespace maci;
 
 extern CALCULATIONS_MODULE::Calculations_var calc_comp;
 
+/* Taken from Astronmical Algorithms, page 135/136 */
 void PrecessionTest::testPrecessionHR() {
 	TYPES::RadecPos posInitial, posFinal;
 
@@ -17,20 +18,21 @@ void PrecessionTest::testPrecessionHR() {
 	CPPUNIT_ASSERT( fabs(posFinal.ra  - 41.547214F) < 0.00001 );
 	CPPUNIT_ASSERT( fabs(posFinal.dec - 49.348483F) < 0.00001 );
 
-	// TODO: Fill these tests with the correct data
-	// See Astronomical Algorithms, exercise on page 136
-	//posInitial.ra  = 37.952937; posInitial.dec = 89.264089;
-	//posFinal = calc_comp->precessionHR(posInitial,2451545.0,);
+	posInitial.ra  = 37.952937 + 0.19877*100*15/3600.; posInitial.dec = 89.264089 - 0.0152*100/3600.;
+	posFinal = calc_comp->precessionHR(posInitial,2451545.0,2451545.0+100*365.25);
+	CPPUNIT_ASSERT( fabs(posFinal.ra  - 88.371542F) < 0.00001 );
+	CPPUNIT_ASSERT( fabs(posFinal.dec - 89.539496F) < 0.00001 );
+
+	posInitial.ra  = 37.952937 + 0.19877*50*15/3600.;  posInitial.dec = 89.264089 - 0.0152*50/3600.;
+	posFinal = calc_comp->precessionHR(posInitial,2451545.0,2451545.0+50*365.25);
+	CPPUNIT_ASSERT( fabs(posFinal.ra  - 57.068458F) < 0.00001 );
+	CPPUNIT_ASSERT( fabs(posFinal.dec - 89.454272F) < 0.00001 );
+
+	// TODO: Write this test with a previous date
+	//posInitial.ra  = 37.952937 + 0.19877*(-99)*15/3600.; posInitial.dec = 89.264089 - 0.0152*(-99)/3600.;
+	//posFinal = calc_comp->precessionHR(posInitial,2451545.0,2415020.3135);
+	//printf("\n%.6f\n",posFinal.ra);
+	//printf("%.6f\n",posFinal.dec);
 	//CPPUNIT_ASSERT( fabs(posFinal.ra  - 20.641250F) < 0.00001 );
 	//CPPUNIT_ASSERT( fabs(posFinal.dec - 88.773939F) < 0.00001 );
-
-	//posInitial.ra  = 37.952937; posInitial.dec = 89.264089;
-	//posFinal = calc_comp->precessionHR(posInitial,2451545.0,);
-	//CPPUNIT_ASSERT( fabs(posFinal.ra  - 41.547214F) < 0.00001 );
-	//CPPUNIT_ASSERT( fabs(posFinal.dec - 49.348483F) < 0.00001 );
-
-	//posInitial.ra  = 37.952937; posInitial.dec = 89.264089;
-	//posFinal = calc_comp->precessionHR(posInitial,2451545.0,);
-	//CPPUNIT_ASSERT( fabs(posFinal.ra  - 41.547214F) < 0.00001 );
-	//CPPUNIT_ASSERT( fabs(posFinal.dec - 49.348483F) < 0.00001 );
 }
