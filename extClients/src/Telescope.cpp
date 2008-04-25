68db8d81a640f30dfbaf86296bdb29c93f738ade
rtobar <rtobar@75a0fe40-ac5b-440f-90d5-6af9f3fbf44f>
2008-04-25 00:36:43 +0000

    We initialize the M/S system only if local
    git-svn-id: https://csrg.inf.utfsm.cl/repos/software/CSAT@747 75a0fe40-ac5b-440f-90d5-6af9f3fbf44f

--- extClients/src/Telescope.cpp
+++ extClients/src/Telescope.cpp
@@ -3,16 +3,21 @@
 Telescope::Telescope(bool isLocal)
 {
 	this->isLocal = isLocal;
-	serial   = "/dev/ttyS0";
-	serialbk = "/dev/ttyS0.bk";
+	this->serial   = "/dev/ttyS0";
+	this->serialbk = "/dev/ttyS0.bk";
 }
 
 Telescope::~Telescope()
 {
-	close(fdm);
-	close(fds);
-	unlink(serial);
-	rename(serialbk,serial);
+	if( this->isLocal ) {
+		close(this->fdm);
+		close(this->fds);
+		unlink(this->serial);
+		rename(this->serialbk,serial);
+	}
+	else {
+		close(this->fds);
+	}
 }
 
 void Telescope::parseInstructions()
@@ -25,35 +30,43 @@ int Telescope::start()
 	//char buf;
 	static struct termios termorig;  /* static for zero's */
 
-	if( (fdm = open("/dev/ptmx", O_RDWR)) < 0)
-		return -1;
-	printf("fdm: %d\n", fdm);
-	grantpt(fdm); // change permission of slave
-	unlockpt(fdm); // unlock slave
-	setsid();
-	if( (slavename = ptsname(fdm)) == NULL) // get name of slave
-	{
-		close(fdm);
-		return -1;
+	if( this->isLocal ) {
+		if( (this->fdm = open("/dev/ptmx", O_RDWR)) < 0)
+			return -1;
+		printf("fdm: %d\n", this->fdm);
+		grantpt(this->fdm); // change permission of slave
+		unlockpt(this->fdm); // unlock slave
+		setsid();
+		if( (slavename = ptsname(this->fdm)) == NULL) // get name of slave
+		{
+			close(this->fdm);
+			return -1;
+		}
+		if( (this->fds = open(slavename, O_RDWR)) < 0) // open slave
+		{
+			close(this->fdm);
+			return -1;
+		}
+		rename(this->serial,this->serialbk);
+		if(symlink(slavename,this->serial)!=0)
+			printf("Unable\n");
+		chmod(slavename, S_IRWXU|S_IRWXG|S_IRWXO);
+		printf("fds: %d\n", this->fds);
+		ioctl(this->fds, I_PUSH, "ptem"); // push ptem
+		ioctl(this->fds, I_PUSH, "ldterm"); // push ldterm
+		if (ioctl(this->fds, TCGETS, &termorig) == -1) {
+			perror("ioctl TCGETS failed");
+			return -1;
+		}
+		termorig.c_lflag = 0;
+		(void) ioctl(this->fds, TCSETS, &termorig);
 	}
-	if( (fds = open(slavename, O_RDWR)) < 0) // open slave
-	{
-		close(fdm);
-		return -1;
+
+	else {
+		slavename = (char *)this->serial;
+		this->fds = open(slavename,O_RDWR);
 	}
-	rename(serial,serialbk);
-	if(symlink(slavename,serial)!=0)
-		printf("Unable\n");
-	chmod(slavename, S_IRWXU|S_IRWXG|S_IRWXO);
-	printf("fds: %d\n", fds);
-	ioctl(fds, I_PUSH, "ptem"); // push ptem
-	ioctl(fds, I_PUSH, "ldterm"); // push ldterm
-	if (ioctl(fds, TCGETS, &termorig) == -1) {
-		perror("ioctl TCGETS failed");
-		return -1;
-	}
-	termorig.c_lflag = 0;
-	(void) ioctl(fds, TCSETS, &termorig);
+
 	printf("%s\n\n", slavename);
 
 	this->parseInstructions();
