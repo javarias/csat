d4164ee5be11dc6d292cb2b5beba7f670509a38d
rtobar <rtobar@75a0fe40-ac5b-440f-90d5-6af9f3fbf44f>
2008-09-02 19:11:56 +0000

    First usage of external lib
    git-svn-id: https://csrg.inf.utfsm.cl/repos/software/CSAT@804 75a0fe40-ac5b-440f-90d5-6af9f3fbf44f

--- amTCS/trunk/src/PointingImpl.cpp
+++ amTCS/trunk/src/PointingImpl.cpp
@@ -1,1 +1,49 @@
+#include <Pointing.h>
+
 #include "PointingImpl.h"
+
+PointingImpl::PointingImpl(const ACE_CString &name, maci::ContainerServices *containerServices) :
+	ACSComponentImpl(name,containerServices)
+{
+
+}
+
+::CORBA::Double PointingImpl::altOffset () throw (::CORBA::SystemException){
+
+}
+
+::CORBA::Double PointingImpl::azmOffset () throw (::CORBA::SystemException){
+
+}
+
+void PointingImpl::offSetAlt (::CORBA::Double degrees) throw (::CORBA::SystemException){
+
+}
+
+void PointingImpl::offSetAzm (::CORBA::Double degrees) throw (::CORBA::SystemException){
+
+}
+
+::TYPES::RadecPos PointingImpl::offSet (const ::TYPES::RadecPos & p) throw (::CORBA::SystemException){
+
+}
+
+void PointingImpl::resetAdjusts () throw(::CORBA::SystemException){
+
+}
+
+void PointingImpl::addObs (const ::TYPES::RadecPos & p_t, const ::TYPES::RadecPos & p_e, ::CORBA::Double sidereal_time) throw(::CORBA::SystemException){
+
+}
+
+void PointingImpl::calculateCoeffs () throw(::CORBA::SystemException){
+
+}
+
+void PointingImpl::setState (::CORBA::Boolean state, ::TYPES::PointingModel model) throw (::CORBA::SystemException){
+
+}
+
+::CORBA::Boolean PointingImpl::getState (::TYPES::PointingModel model) throw (::CORBA::SystemException){
+
+}
